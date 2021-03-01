30fee76fd974ac2d2aeaec6d03856374ee4c0aa9
Major Hayden <major@mhtx.net>
2021-03-01 14:19:46 -0600

    Add logging to thetastreak
    Signed-off-by: Major Hayden <major@mhtx.net>

--- thetastreak/thetastreak.py
+++ thetastreak/thetastreak.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 import os
 import time
 
@@ -12,6 +13,8 @@ TG_PASSWORD = os.environ.get("TG_PASSWORD")
 chrome_options = Options()
 chrome_options.add_argument("headless")
 driver = webdriver.Chrome(options=chrome_options)
+
+logging.info("Loading the site...")
 driver.get("https://thetagang.com/login")
 
 time.sleep(5)
@@ -24,13 +27,16 @@ elem = driver.find_element_by_xpath('//*[@id="Password"]')
 elem.clear()
 elem.send_keys(TG_PASSWORD)
 
+logging.info("Submitting login...")
 elem.send_keys(Keys.RETURN)
 
 time.sleep(5)
 
 assert TG_USERNAME in driver.page_source
+
+logging.info("Refresh the page...")
 driver.refresh()
 
 time.sleep(1)
-
 driver.close()
+logging.info("Done! 🎉")
