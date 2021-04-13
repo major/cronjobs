bfd461f0be939c997ad2accf55ccb3f50a653613
Major Hayden <major@mhtx.net>
2021-04-13 07:39:06 -0500

    Update thetastreak for new thetagang adjustments

--- thetastreak/thetastreak.py
+++ thetastreak/thetastreak.py
@@ -6,6 +6,18 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def wait_for_load(driver):
+    """Wait for the page to be fully loaded."""
+    time.sleep(5)
+    WebDriverWait(driver=driver, timeout=10).until(
+        lambda x: x.execute_script(
+            "return document.readyState === 'complete'"
+        )
+    )
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,13 +25,18 @@ TG_USERNAME = os.environ.get("TG_USERNAME")
 TG_PASSWORD = os.environ.get("TG_PASSWORD")
 
 chrome_options = Options()
-chrome_options.add_argument("headless")
+# chrome_options.add_argument("headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 logging.info("Loading the site...")
 driver.get("https://thetagang.com/login")
 
-time.sleep(5)
+wait_for_load(driver)
+
+# Clear the modal.
+driver.refresh()
+
+wait_for_load(driver)
 
 elem = driver.find_element_by_xpath('//*[@id="Username"]')
 elem.clear()
@@ -30,15 +47,19 @@ elem.clear()
 elem.send_keys(TG_PASSWORD)
 
 logging.info("Submitting login...")
-elem.send_keys(Keys.RETURN)
+elem.send_keys(Keys.ENTER)
 
-time.sleep(5)
-
-assert TG_USERNAME in driver.page_source
+wait_for_load(driver)
 
 logging.info("Refresh the page...")
 driver.refresh()
 
-time.sleep(1)
+wait_for_load(driver)
+
+premium_div = driver.find_element_by_xpath(
+    '//*[@id="root"]/div/div/div/div[3]/div[1]/div[1]/div[3]/div/div[4]/div[2]'
+)
+print(premium_div.text)
+
 driver.close()
 logging.info("Done! 🎉")
