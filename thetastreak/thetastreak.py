b57a367232829eaa7d0d1082c776fde4c09b3a25
Major Hayden <major@mhtx.net>
2023-04-18 10:26:48 -0500

    Align thetastreak with new v3 site

--- thetastreak/thetastreak.py
+++ thetastreak/thetastreak.py
@@ -30,24 +30,14 @@ def run(playwright: Playwright) -> None:
     # Fill [placeholder="password"]
     page.locator('[placeholder="password"]').fill(TG_PASSWORD)
 
-    # Click text=Sign In
-    page.locator("text=Login").click()
-
-    # Click text=Sign In
-    # with page.expect_navigation(url="https://thetagang.com/"):
-    with page.expect_navigation():
-        page.locator("text=Login").click()
+    # Click login button
+    page.get_by_role("button", name="Login").click()
 
     # Click text=mhayden >> nth=1
     page.locator("text=mhayden").nth(1).click()
-    # assert page.url == "https://thetagang.com/mhayden"
 
     # Click text=Open Trades
     page.locator("text=Open Trades").click()
-
-    # Click img
-    page.locator("img").click()
-    # assert page.url == "https://thetagang.com/"
 
     # Close page
     page.close()
